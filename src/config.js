b3d40426b740918f86e037dbdfef323c1a26db96
imvinutha <ivinutha99@gmail.com>
2025-12-23 13:04:00 +0530

    Update backend url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,14 @@
+// // API URL configuration
+// // export const API_URL = 'http://192.168.0.105:5000';
+// export const API_URL = "https://locker-system-backend-cxeu.onrender.com";
+
+// // Locker hardware API URL
+// // export const LOCKER_API_URL = 'http://192.168.0.105';
+// export const LOCKER_API_URL = "https://locker-system-backend-cxeu.onrender.com";
+
+
 // API URL configuration
-// export const API_URL = 'http://192.168.0.105:5000';
-export const API_URL = "https://locker-system-backend-cxeu.onrender.com";
+export const API_URL = "https://locker-system-backendd.onrender.com";
 
 // Locker hardware API URL
-// export const LOCKER_API_URL = 'http://192.168.0.105';
-export const LOCKER_API_URL = "https://locker-system-backend-cxeu.onrender.com";
+export const LOCKER_API_URL = "https://locker-system-backendd.onrender.com";
